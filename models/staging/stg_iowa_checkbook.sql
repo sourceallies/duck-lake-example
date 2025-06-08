de9f0ad3ded0d3997faaf45fba8b93f5dc884b05
Paul Rowe <prowe@sourceallies.com>
2025-06-07 21:28:15 -0500

    fixing data source

--- models/staging/stg_iowa_checkbook.sql
+++ models/staging/stg_iowa_checkbook.sql
@@ -1,3 +1,3 @@
 select *
-from 'https://data.iowa.gov/resource/cyqb-8ina.csv?fiscal_year=2020&fiscal_year_period=1&$limit=1000000000'
--- from '/Users/prowe/Development/modern-data-stack/jaffle_shop_duckdb/checkbook.csv'
+-- from 'https://data.iowa.gov/resource/cyqb-8ina.csv?fiscal_year=2020&fiscal_year_period=1&$limit=1000000000'
+from './checkbook.csv'
